c22881d7ecb9b1c6e7da485d2c778e8775024912
Annabel Church <bells.stuff+gitcommitme@gmail.com>
2015-08-03 19:19:23 +0200

    What a graph service would look like

---
+++ src/app/components/graph/graph.service.js
@@ -0,0 +1,63 @@
+'use strict';
+
+angular.module('ttip.graph').
+  service('dataService', function($http, $q) {
+
+    function getNodes() {
+        var request = $http({
+            method: 'get',
+            url: 'data/ttip_popolo_en.json'
+        });
+        return( request.then( handleSuccess, handleError ) );
+    }
+
+    function addNodes(  ) {
+        var request = $http({
+            method: 'post',
+            url: ''
+            // params: {
+            //     action: 'add'
+            // },
+            // data: {
+            //     name: name
+            // }
+        });
+        return( request.then( handleSuccess, handleError ) );
+    }
+
+    function removeNode( id ) {
+        var request = $http({
+            method: 'delete',
+            url: '',
+            // params: {
+            //     action: 'delete'
+            // },
+            data: {
+                id: id
+            }
+        });
+        return( request.then( handleSuccess, handleError ) );
+    }
+
+    function handleError( response ) {
+        // Normalize it on our end, as best we can.
+        if (
+            ! angular.isObject( response.data ) ||
+            ! response.data.message
+            ) {
+            return( $q.reject( 'An unknown error occurred.' ) );
+        }
+        // Expected error message.
+        return( $q.reject( response.data.message ) );
+    }
+
+    function handleSuccess( response ) {
+        return( response.data );
+    }
+
+    return({
+        addNodes: addNodes,
+        getNodes: getNodes,
+        removeNode: removeNode
+    });
+});
