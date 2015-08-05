d19848032aded7d9872190dcb694bdf1cb01ce2b
Annabel Church <bells.stuff+gitcommitme@gmail.com>
2015-08-05 22:11:53 +0200

    oh how did that happen. remove comment

--- src/app/components/graph/graph.directive.js
+++ src/app/components/graph/graph.directive.js
@@ -66,37 +66,37 @@ angular.module('ttip.graph')
             .links(data.links)
             .start();
 
-          // var link = svg.selectAll('.link')
-          //   .data(data.links)
-          //   .enter().append('line')
-          //   .attr('class', 'link');
+          var link = svg.selectAll('.link')
+            .data(data.links)
+            .enter().append('line')
+            .attr('class', 'link');
 
-          // var node = svg.selectAll('.node')
-          //   .data(data.nodes)
-          //   .enter().append('g')
-          //   .attr('class', 'node')
-          //   .call(force.drag);
+          var node = svg.selectAll('.node')
+            .data(data.nodes)
+            .enter().append('g')
+            .attr('class', 'node')
+            .call(force.drag);
 
-          // node.append('image')
-          //   .attr('xlink:href', 'https://source.opennews.org/static/base/img/favicon.ico')
-          //   .attr('x', -8)
-          //   .attr('y', -8)
-          //   .attr('width', 16)
-          //   .attr('height', 16);
+          node.append('image')
+            .attr('xlink:href', 'https://source.opennews.org/static/base/img/favicon.ico')
+            .attr('x', -8)
+            .attr('y', -8)
+            .attr('width', 16)
+            .attr('height', 16);
 
-          // node.append('text')
-          //   .attr('dx', 12)
-          //   .attr('dy', '.35em')
-          //   .text(function(d) { return d.name; });
+          node.append('text')
+            .attr('dx', 12)
+            .attr('dy', '.35em')
+            .text(function(d) { return d.name; });
 
-          // force.on('tick', function() {
-          //   link.attr('x1', function(d) { return d.source.x; })
-          //       .attr('y1', function(d) { return d.source.y; })
-          //       .attr('x2', function(d) { return d.target.x; })
-          //       .attr('y2', function(d) { return d.target.y; });
+          force.on('tick', function() {
+            link.attr('x1', function(d) { return d.source.x; })
+                .attr('y1', function(d) { return d.source.y; })
+                .attr('x2', function(d) { return d.target.x; })
+                .attr('y2', function(d) { return d.target.y; });
 
-          //   node.attr('transform', function(d) { return 'translate(' + d.x + ',' + d.y + ')'; });
-          // });
+            node.attr('transform', function(d) { return 'translate(' + d.x + ',' + d.y + ')'; });
+          });
         }
 
         drawGraph();
